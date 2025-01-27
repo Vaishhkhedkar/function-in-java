154fe8ee1cd9f9319cf5aacc15ac1f66d1d85550
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-01-27 19:08:41 +0530

    fun.without argument without returning value
    class woargworet
    {
        static void Add()
        {
            int a=5,b=7,c;
            c=a+b;
            System.out.println("Add="+c);
        }
        public static void main(String args [])
    
        {
         Add();
        }
    
    }

---
+++ woargworet.java
@@ -0,0 +1,15 @@
+class woargworet
+{
+    static void Add()
+    {
+        int a=5,b=7,c;
+        c=a+b;
+        System.out.println("Add="+c);
+    }
+    public static void main(String args [])
+
+    {
+     Add();
+    }
+
+}

0d6f5248551a0149028f111f10eb323dc691c6cc
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-01-27 20:57:09 +0530

    function with argument without returning
    class fwargworet
    {
        static void Add(int a,int b)
        {
            int c;
            c=a+b;
            System.out.println("Add="+c);
        }
        public static void main(String args [])
    
        {
         Add(60,99);
        }
    
    }

---
+++ fwargworet.java
@@ -0,0 +1,15 @@
+class fwargworet
+{
+    static void Add(int a,int b)
+    {
+        int c;
+        c=a+b;
+        System.out.println("Add="+c);
+    }
+    public static void main(String args [])
+
+    {
+     Add(60,99);
+    }
+
+}
